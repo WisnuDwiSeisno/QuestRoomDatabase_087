cc1f482820eb4e47e26baae2a68df2a688024636
WisnuDwiSeisno <wseisno37@gmail.com>
2024-12-16 21:14:08 +0700

    fungsi untuk mendapatkan semua data Mahasiswa dan data Mahasiswa berdasarkan NIM

--- app/src/main/java/com/example/act9/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/act9/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.act9.repository
 
 import com.example.act9.data.dao.MahasiswaDao
 import com.example.act9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
@@ -9,4 +10,8 @@ class LocalRepositoryMhs(
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+    override fun getAllMhs(): Flow<List<Mahasiswa>> =
+        mahasiswaDao.getAllMahasiswa()
+    override fun getMhs(nim: String): Flow<Mahasiswa> =
+        mahasiswaDao.getMahasiswa(nim)
 }
--- app/src/main/java/com/example/act9/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/act9/repository/RepositoryMhs.kt
@@ -1,7 +1,10 @@
 package com.example.act9.repository
 
 import com.example.act9.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+    fun getMhs(nim: String): Flow<Mahasiswa>
 }
