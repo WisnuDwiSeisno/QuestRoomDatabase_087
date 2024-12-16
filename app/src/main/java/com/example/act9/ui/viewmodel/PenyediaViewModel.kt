52a2b95bb12c42eb0cf5896b2cbea8f161293da7
WisnuDwiSeisno <wseisno37@gmail.com>
2024-12-16 22:28:55 +0700

    menambahkan objek PenyediaViewModel untuk menyediakan berbagai ViewModel dengan factory, termasuk MahasiswaViewModel, HomeMhsViewModel, DetailMhsViewModel, dan UpdateMhsViewModel

--- app/src/main/java/com/example/act9/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/act9/ui/viewmodel/PenyediaViewModel.kt
@@ -1,16 +1,37 @@
 package com.example.roomlocaldb.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.act9.KrsApp
+import com.example.act9.ui.viewmodel.DetailMhsViewModel
+import com.example.act9.ui.viewmodel.HomeMhsViewModel
 import com.example.act9.ui.viewmodel.MahasiswaViewModel
+import com.example.act9.ui.viewmodel.UpdateMhsViewModel
 
-object PenyediaViewModel{
+object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
